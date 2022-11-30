947148ca7720a41ca9f6279ad137d6b558e91292
Cam92 <cameronjm92@googlemail.com>
2022-11-30 15:11:18 +0000

    Uploading old learning projects

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 async function main() {
   // replace argument with the name of the contract you want to deploy!
-  const Game = await hre.ethers.getContractFactory("Game1");
+  const Game = await hre.ethers.getContractFactory("Game4");
   // if you need to add constructor arguments for the game, add them here:
-  const game = await Game.deploy();
+  const game = await Game.deploy(123);
   console.log(`Game deployed to address: `, game.address);
 }
 
--- scripts/win.js
+++ scripts/win.js
@@ -1,9 +1,17 @@
-const GAME_ADDRESS = "";
+const { ethers } = require("ethers");
+
+const GAME_ADDRESS = "0x4D95186c314f362eF74550ce970a1aAd949e1075";
+const URL = process.env.RINKEBY_URL;
+const provider = new ethers.providers.JsonRpcProvider(URL);
 
 async function main() {
-  const game = await hre.ethers.getContractAt("Game1", GAME_ADDRESS);
+  const game = await hre.ethers.getContractAt("Game4", GAME_ADDRESS);
 
-  const tx = await game.win();
+  const secret = await provider.getStorageAt(GAME_ADDRESS, "0xd367c13b6472f77e7a5dda1c6168dc4ec604d3a1b3b5f06a8197270887866b4c");
+
+  console.log(secret);
+
+  const tx = await game.win(secret);
   const receipt = await tx.wait();
 
   console.log(receipt);
